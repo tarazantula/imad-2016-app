871222b7b35893ead2091a203d31073d7536a93d
tarazantula <taragayatri@gmail.com>
2016-11-19 20:57:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/IMG_20161023_161150375.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'IMG_20161023_161150375.jpg'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
